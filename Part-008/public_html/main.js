42b7a1e2b9fd425678e2499965a9d0102e45305d
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-05-28 21:45:37 +0200

    js/libs

---
+++ Part-008/public_html/main.js
@@ -0,0 +1,12 @@
+require.config({
+    baseUrl: './',
+    paths: {
+        knockout: 'js/libs/knockout/dist/knockout',
+        text: 'js/libs/text/text',
+        app: 'js/viewModels/app'
+    },
+    waitSeconds: 2
+});
+require(['knockout', 'app'], function (ko, app, text) {
+    ko.applyBindings(new app());
+});
