25641055d238d26e291e763342e80e56c4d903c4
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-05-28 21:40:07 +0200

    js/libs

--- Part-005/public_html/main.js
+++ Part-005/public_html/main.js
@@ -1,7 +1,7 @@
 require.config({
     baseUrl: './',
     paths: {
-        knockout: 'bower_components/knockout/dist/knockout',
+        knockout: 'js/libs/knockout/dist/knockout',
         app: 'js/viewModels/app'
     },
     waitSeconds: 2
