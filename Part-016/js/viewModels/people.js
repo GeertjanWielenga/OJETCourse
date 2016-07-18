883d43775ef083ebe656eebb69d3b409428ac8ad
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-07-18 13:06:37 +0200

    Starting point for converters and validators.

--- Part-016/js/viewModels/people.js
+++ Part-016/js/viewModels/people.js
@@ -1,9 +1,7 @@
-/**
- * Copyright (c) 2014, 2016, Oracle and/or its affiliates.
- * The Universal Permissive License (UPL), Version 1.0
- */
-/* 
- * Your viewModel code goes here
- */
-
-
+define(['ojs/ojcore', 'knockout'],
+    function (oj, ko) {
+        function peopleContentViewModel() {
+            var self = this;
+        }
+        return new peopleContentViewModel();
+    });
