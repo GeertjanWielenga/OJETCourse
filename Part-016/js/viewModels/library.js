259a57573aac057d75382d1cf4d8902c60fa6133
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-07-18 16:43:10 +0200

    Starting point for converters and validators.

---
+++ Part-016/js/viewModels/library.js
@@ -0,0 +1,8 @@
+define(['ojs/ojcore', 'knockout'],
+    function (oj, ko) {
+        function libraryContentViewModel() {
+            var self = this;
+
+        }
+        return new libraryContentViewModel();
+    });
