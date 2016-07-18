85d613a98352a99f0d4e3bc398824b25f325f1c6
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-07-18 16:24:09 +0200

    Starting point for converters and validators.

--- Part-016/README.md
+++ Part-016/README.md
@@ -6,4 +6,8 @@ How to use this sample:
 
 1. Open the sample.
 
-2. 
+2. In the 'home' module, discuss the datepicker component, with its converters, 
+   and its apearance, and compare it to the standard HTML5 element.
+
+3. In the 'people' module, discuss the datepicker component, with its converters, 
+   and its apearance, and compare it to the standard HTML5 element.
--- Part-016/js/viewModels/people.js
+++ Part-016/js/viewModels/people.js
@@ -1,7 +1,10 @@
-define(['ojs/ojcore', 'knockout'],
+define(['ojs/ojcore', 'knockout', 'ojs/ojdatetimepicker'],
     function (oj, ko) {
         function peopleContentViewModel() {
             var self = this;
+            self.date = ko.observable();
+            self.todayIsoDate = ko.observable(oj.IntlConverterUtils.dateToLocalIso(new Date()));
+            self.milleniumStartIsoDate = ko.observable(oj.IntlConverterUtils.dateToLocalIso(new Date(2000, 00, 01)));
         }
         return new peopleContentViewModel();
     });
