d5062141bc1c3a89958db530e167b774156ced20
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-07-24 22:53:09 +0200

    Tweaking so that buyer.js is found in a different folder.

--- Part-004/public_html/js/appViewModel.js
+++
@@ -1,8 +0,0 @@
-define(['knockout'], function (ko) {
-    return function appViewModel() {
-        this.firstName = ko.observable('Bert');
-        this.firstNameCaps = ko.pureComputed(function() {
-            return this.firstName().toUpperCase();
-        }, this);
-    };
-});
