0b320d1b650a04ca9200dd439abe9ccde8156767
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-07-24 21:58:59 +0200

    Tweaking so that buyer.js is found in a different folder.

--- Part-002/public_html/js/main.js
+++ Part-002/public_html/js/main.js
@@ -1,4 +1,4 @@
-require(["js/buyer"], function (buyer) {
+require(["buyer"], function (buyer) {
     name = 'John Smith';
     console.log(name + " enters the system");
     var result = buyer.buyThing(name);
