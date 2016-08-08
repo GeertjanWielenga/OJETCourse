d2c7060302cd7e2388805c09cbe6aed3414d2849
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-08-08 12:18:23 +0200

    Part-17

--- Part-017/js/viewModels/library.js
+++ Part-017/js/viewModels/library.js
@@ -1,9 +1,17 @@
-/**
- * Copyright (c) 2014, 2016, Oracle and/or its affiliates.
- * The Universal Permissive License (UPL), Version 1.0
- */
-/* 
- * Your viewModel code goes here
- */
-
-
+define(['ojs/ojcore', 'knockout', 'ojs/ojmasonrylayout'],
+    function (oj, ko) {
+        function libraryContentViewModel() {
+            var self = this;
+            self.chemicals = [
+                {name: 'Hydrogen', sizeClass: 'oj-masonrylayout-tile-2x1'},
+                {name: 'Helium', sizeClass: 'oj-masonrylayout-tile-1x1'},
+                {name: 'Lithium', sizeClass: 'oj-masonrylayout-tile-1x1'},
+                {name: 'Beryllium', sizeClass: 'oj-masonrylayout-tile-1x1'},
+                {name: 'Boron', sizeClass: 'oj-masonrylayout-tile-1x1'},
+                {name: 'Carbon', sizeClass: 'oj-masonrylayout-tile-1x1'},
+                {name: 'Nitrogen', sizeClass: 'oj-masonrylayout-tile-1x1'},
+                {name: 'Oxygen', sizeClass: 'oj-masonrylayout-tile-1x1'}
+            ];
+        }
+        return new libraryContentViewModel();
+    });
