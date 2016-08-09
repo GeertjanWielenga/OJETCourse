0b5bfe5c9e2317147305fd74c3128d5d933c96c4
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-08-09 08:31:15 +0200

    Part-18

--- Part-018/public_html/js/viewModels/footer.js
+++ Part-018/public_html/js/viewModels/footer.js
@@ -1,17 +1,5 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * footer module
- */
 define(['ojs/ojcore', 'knockout'
 ], function (oj, ko) {
-    /**
-     * The view model for the main content view template
-     */
     function footerContentViewModel() {
         var self = this;
     }
