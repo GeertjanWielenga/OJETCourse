f287862c75445bbdf613d810bca12ad8d7a32cdc
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-10-25 18:28:46 +0200

    changing '2.2.0' to 'latest'

--- Part-008/public_html/js/main.js
+++ Part-008/public_html/js/main.js
@@ -4,33 +4,47 @@
 
 requirejs.config({
     // Path mappings for the logical module names
-paths: {
-    'knockout': 'libs/knockout/dist/knockout',
-    'jquery': 'libs/jquery/dist/jquery.min',
-    'jqueryui-amd': 'libs/jquery-ui/ui',
-    'promise': 'libs/es6-promise/promise.min',
-    'hammerjs': 'libs/hammerjs/hammer.min',
-    'ojdnd': 'libs/oraclejet/dist/js/libs/dnd-polyfill/dnd-polyfill-1.0.0.min',
-    'ojs': 'libs/oraclejet/dist/js/libs/oj/min',
-    'ojL10n': 'libs/oraclejet/dist/js/libs/oj/ojL10n',
-    'ojtranslations': 'libs/oraclejet/dist/js/libs/oj/resources',
-    'text': 'libs/text/text',
-    'signals': 'libs/js-signals/dist/signals.min'
-},
+    paths: {
+        'knockout': 'libs/knockout/dist/knockout',
+        'jquery': 'libs/jquery/dist/jquery.min',
+        'jqueryui-amd': 'libs/jquery-ui/ui',
+        'promise': 'libs/es6-promise/promise.min',
+        'hammerjs': 'libs/hammerjs/hammer.min',
+        'ojdnd': 'libs/oraclejet/dist/js/libs/dnd-polyfill/dnd-polyfill-1.0.0.min',
+        'ojs': 'libs/oraclejet/dist/js/libs/oj/min',
+        'ojL10n': 'libs/oraclejet/dist/js/libs/oj/ojL10n',
+        'ojtranslations': 'libs/oraclejet/dist/js/libs/oj/resources',
+        'text': 'libs/text/text',
+        'signals': 'libs/js-signals/dist/signals.min'
+    },
     // Shim configurations for modules that do not expose AMD
     shim: {
         'jquery': {
             exports: ['jQuery', '$']
         }
     },
-// This section configures the i18n plugin. It is merging the Oracle JET built-in translation
-// resources with a custom translation file.
-// Any resource file added, must be placed under a directory named "nls". You can use a path mapping or you can define
-// a path that is relative to the location of this main.js file.
+
+    // This section configures the i18n plugin. It is merging the Oracle JET built-in translation
+    // resources with a custom translation file.
+    // Any resource file added, must be placed under a directory named "nls". You can use a path mapping or you can define
+    // a path that is relative to the location of this main.js file.
     config: {
         ojL10n: {
             merge: {
                 //'ojtranslations/nls/ojtranslations': 'resources/nls/myTranslations'
+            }
+        },
+        text: {
+            // Override for the requirejs text plugin XHR call for loading text resources on CORS configured servers
+            useXhr: function (url, protocol, hostname, port) {
+                // Override function for determining if XHR should be used.
+                // url: the URL being requested
+                // protocol: protocol of page text.js is running on
+                // hostname: hostname of page text.js is running on
+                // port: port of page text.js is running on
+                // Use protocol, hostname, and port to compare against the url being requested.
+                // Return true or false. true means "use xhr", false means "fetch the .js version of this resource".
+                return true;
             }
         }
     }
@@ -57,7 +71,6 @@ require(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojmodule', '
                         ko.applyBindings(new DemoViewModel());
                     }
             );
-
         }
 );
 
