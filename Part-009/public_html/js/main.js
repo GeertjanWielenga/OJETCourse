3e69626ae61bc8bfa683151f90a756546e65873f
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-10-25 11:39:27 +0200

    Updating bower.json and main.js.

--- Part-009/public_html/js/main.js
+++ Part-009/public_html/js/main.js
@@ -8,14 +8,14 @@ paths: {
     'knockout': 'libs/knockout/dist/knockout',
     'jquery': 'libs/jquery/dist/jquery.min',
     'jqueryui-amd': 'libs/jquery-ui/ui',
+    'promise': 'libs/es6-promise/promise.min',
+    'hammerjs': 'libs/hammerjs/hammer.min',
+    'ojdnd': 'libs/oraclejet/dist/js/libs/dnd-polyfill/dnd-polyfill-1.0.0.min',
     'ojs': 'libs/oraclejet/dist/js/libs/oj/min',
     'ojL10n': 'libs/oraclejet/dist/js/libs/oj/ojL10n',
     'ojtranslations': 'libs/oraclejet/dist/js/libs/oj/resources',
     'text': 'libs/text/text',
-    'promise': 'libs/es6-promise/promise.min',
-    'hammerjs': 'libs/hammerjs/hammer.min',
-    'signals': 'libs/js-signals/dist/signals.min',
-    'ojdnd': 'libs/oraclejet/dist/libs/dnd-polyfill/dnd-polyfill-1.0.0.min'
+    'signals': 'libs/js-signals/dist/signals.min'
 },
     // Shim configurations for modules that do not expose AMD
     shim: {
