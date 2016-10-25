e73e7e405e7293f64180248955704a0bdd65e083
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-10-25 19:47:46 +0200

    updating to latest

--- Part-016/js/viewModels/graphics.js
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) 2014, 2016, Oracle and/or its affiliates.
- * The Universal Permissive License (UPL), Version 1.0
- */
-/* 
- * Your viewModel code goes here
- */
-
-
--- Part-016/js/viewModels/home.js
+++
@@ -1,8 +0,0 @@
-define(['ojs/ojcore', 'knockout', 'ojs/ojdatetimepicker'],
-    function (oj, ko) {
-        function mainContentViewModel() {
-            var self = this;
-            self.date = ko.observable();
-        }
-        return new mainContentViewModel();
-    });
