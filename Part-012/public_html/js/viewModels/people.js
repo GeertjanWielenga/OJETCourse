28d07dafcab724ae3c0747e11de2bd6f08c43d05
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-10-26 10:38:17 +0200

    Adding gauge sample.

--- Part-012/public_html/js/viewModels/people.js
+++ Part-012/public_html/js/viewModels/people.js
@@ -1,7 +1,8 @@
-define(['ojs/ojcore', 'knockout'
+define(['ojs/ojcore', 'knockout', 'ojs/ojgauge'
 ], function (oj, ko) {
     function peopleContentViewModel() {
         var self = this;
+        self.value10 = ko.observable(80);
     }
     return peopleContentViewModel;
 });
