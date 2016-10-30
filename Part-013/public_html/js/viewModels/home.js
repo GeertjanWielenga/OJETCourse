1ba3dc831fbe5bc84e9558c1fcc86d4ea4315cc9
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-10-30 14:08:11 +0100

    starting point of part 13 sample code

--- Part-013/public_html/js/viewModels/home.js
+++ Part-013/public_html/js/viewModels/home.js
@@ -1,8 +1,24 @@
-define(['ojs/ojcore', 'knockout', 'ojs/ojtable'
-], function (oj, ko) {
-    function homeContentViewModel() {
-        var self = this;
-        self.data = ko.observableArray();
-    }
-    return homeContentViewModel;
+define(['ojs/ojcore', 'knockout', 'ojs/ojknockout', 'ojs/ojtable'
+   ], function(oj, ko) {
+        function homeContentViewModel() {
+            var self = this;
+            var deptArray = [
+                {DepartmentId: 10, DepartmentName: 'History'},
+                {DepartmentId: 20, DepartmentName: 'Geography'},
+                {DepartmentId: 30, DepartmentName: 'Biology'}];
+            self.datasource =
+                    new oj.ArrayTableDataSource(
+                            deptArray,
+                            {idAttribute: 'DepartmentId'});
+            self.currentRowListener = function (event, ui) {
+                var newCurrentRow = ui.currentRow;
+                self.datasource.at(newCurrentRow['rowIndex']).
+                        then(function (rowObj) {
+                            var obj = rowObj['data'];
+                            $('#selectedDepartmentId').text(obj.DepartmentId);
+                            $('#selectedDepartmentName').text(obj.DepartmentName);
+                        });
+            };
+        }
+   return homeContentViewModel;
 });
--- Part-013/public_html/js/viewModels/people.js
+++ Part-013/public_html/js/viewModels/people.js
@@ -1,6 +1,35 @@
-define(['ojs/ojcore', 'knockout', 'ojs/ojtable'],
-function (oj, ko, MovieFactory) {
-    var viewModel = {
+define(['ojs/ojcore', 'knockout', 'ojs/ojknockout', 'ojs/ojtree'
+], function (oj, ko) {
+    function homeContentViewModel() {
+        var self = this;
+        self.datasource = [
+            {"title": "Home", "attr": {"id": "home"}, 
+                "children": [
+                    {"title": "Overview", "attr": {"id": "overview"}},
+                    {"title": "Section1", "attr": {"id": "section1"}},
+                    {"title": "Section2", "attr": {"id": "section2"}}
+                ]
+            },
+            {"title": "News", "attr": {"id": "news"}},
+            {"title": 'Contact', "attr": {"id": "contact"}},
+            {"title": 'About', "attr": {"id": "about"}}
+        ];
+        self.currentRowListener = function (event, ui) {
+            if (ui.func === 'select') {
+                var selectedNode = _arrayToStr(ui.item);
+                $('#selectedNode').text(selectedNode);
+            }
+        };
+    }
+    function _arrayToStr(arr) {
+        var s = "";
+        $.each(arr, function (i, val) {
+            if (i) {
+                s += ", ";
+            }
+            s += $(arr[i]).attr("id");
+        });
+        return s;
     };
-    return viewModel;
+    return homeContentViewModel;
 });
